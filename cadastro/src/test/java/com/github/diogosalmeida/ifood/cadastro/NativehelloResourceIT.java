1f82d8a465694af97be7635625584f01e3dce4f5
diogosalmeida <diogodsalmeida@gmail.com>
2021-10-28 00:22:54 -0300

    Config initial

---
+++ cadastro/src/test/java/com/github/diogosalmeida/ifood/cadastro/NativehelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.diogosalmeida.ifood.cadastro;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativehelloResourceIT extends helloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
